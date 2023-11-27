ed094695201647a50a2cc4644f8bdef77212ee42
root <enangita@gmail.com>
2023-11-27 13:57:00 -0800

    check

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming" \
       " language that combines remarkable power with very clear syntax"
-print(" ".join(str.split()[5:10]))
+print(" ".join(str.split()[5:7]) + " " + str.split()[12] + " " + str.split()[0])
