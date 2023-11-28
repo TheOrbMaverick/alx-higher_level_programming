26b931de30b5b15de6328f41d14fe4372e15d0ac
root <enangita@gmail.com>
2023-11-28 15:08:38 -0800

    hm

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('a') - 1, -1):
     case = 'a' if char % 2 == 0 else 'A'
-    print(f"{chr(char - ord('z') + ord('Z') if case == 'A' else char)}", end="")
+    print(f"{}".format(chr(char - ord('z') + ord('Z') if case == 'A' else char)), end="")
