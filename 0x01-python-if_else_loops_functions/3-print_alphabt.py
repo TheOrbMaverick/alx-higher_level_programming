809681c271b744a4969c39b7b26c24e65a7a7c5a
root <enangita@gmail.com>
2023-11-28 11:32:21 -0800

    checks

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
-alphabet = ''.join(chr(i) for i in range(97, 123))
-print("{}".format(alphabet), end='')
+for i in range(97, 123):
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{}".format(chr(i)), end='')
