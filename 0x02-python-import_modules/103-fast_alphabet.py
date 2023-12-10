7bd9e10579ae10119332360640e7e60144c48da3
root <enangita@gmail.com>
2023-12-10 06:48:08 -0800

    whew

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-import string; print(string.ascii_uppercase.translate(str.maketrans("", "", "")))
+if __name__ == "__main__":
+    import string; print(string.ascii_uppercase.translate(str.maketrans("", "", "")))
