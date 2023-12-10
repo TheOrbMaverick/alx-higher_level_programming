85c25c069c0aa73b479d28e6f4a63df31a1604da
root <enangita@gmail.com>
2023-12-10 05:29:44 -0800

    solutions finally

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 
-argc = len(argv) - 1
-plural_s = 's' if argc != 1 else ''
-end_char = ':' if argc != 0 else '.'
+if name == "__main__":
+    argc = len(argv) - 1
+    plural_s = 's' if argc != 1 else ''
+    end_char = ':' if argc != 0 else '.'
 
-print("Number of argument{}{}{}".format(plural_s, 's' if argc else '', end_char))
+    print("Number of argument{}{}{}".format(plural_s, 's' if argc else '', end_char))
 
-for i, arg in enumerate(argv[1:], start=1):
-    print("{}: {}".format(i, arg))
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{}: {}".format(i, arg))
