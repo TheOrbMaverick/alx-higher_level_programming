38b5f10a18621e36579d961bb204a7d8ae19c069
root <enangita@gmail.com>
2023-12-19 12:41:15 -0800

    chi

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -6,12 +6,10 @@ A class of nothing.
 
 class Square:
 
+    def __init__(self,):
+        pass
     """
     This is an empty class Square.
+    pass: just does nothing but jumps over the function.
     """
-
     pass
-
-    """
-    pass signifies that the class is empty.
-    """
