b936f6d5624a9757a7f57ace8457661cd367e8ad
Ita Enang <enangita@gmail.com>
2024-02-13 11:57:36 -0800

    checking

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,12 +1,6 @@
 -- Script to list all tables of a specified database in MySQL server
 
--- Check if the correct database name is provided
--- Note: This script does not handle command-line arguments directly; the database name must be passed when executing the script using a shell command
-
--- Define a placeholder for the database name using a variable
-SET @database_name = '$$DATABASE_NAME$$';
-
 -- Query to list tables
 -- Retrieve table names from the information_schema.tables view
 -- Filter by the specified database name
-SELECT table_name FROM information_schema.tables WHERE table_schema = @database_name;
+SELECT table_name FROM information_schema.tables WHERE table_schema = '$$DATABASE_NAME$$';
