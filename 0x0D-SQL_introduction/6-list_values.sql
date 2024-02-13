0ebfd318b97159657d1e9de64527cd04d982cf96
Ita Enang <enangita@gmail.com>
2024-02-13 12:43:46 -0800

    checking

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,8 @@
+-- This script lists all rows of the table first_table from the database hbtn_0c_0
+-- The database name will be passed as an argument of the mysql command
+
+-- You can run this script using the cat command with mysql like this:
+-- cat 6-list_values.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+
+-- Select all fields from the first_table
+SELECT * FROM hbtn_0c_0.first_table;
