736b6d478541fb4fa258932954658fd791fae8b9
Ita Enang <enangita@gmail.com>
2024-02-13 12:02:08 -0800

    ugh

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -3,4 +3,4 @@
 -- Query to list tables
 -- Retrieve table names from the information_schema.tables view
 -- Filter by the specified database name
-SELECT table_name FROM information_schema.tables WHERE table_schema = '$$DATABASE_NAME$$';
+SELECT table_name FROM information_schema.tables WHERE table_schema = 'mysql';
