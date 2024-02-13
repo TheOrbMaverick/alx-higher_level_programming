003e577604c5a7c67c77f5b8aeb4e6230cb467cc
Ita Enang <enangita@gmail.com>
2024-02-13 11:03:08 -0800

    creating database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Script to create the database hbtn_0c_0 in MySQL server
+-- The script will ignore errors if the database already exists
+
+-- Check if the database hbtn_0c_0 exists, if not, create it
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
