ac9d934014f28dd834a7ce17972505f44fc4848c
Ita Enang <enangita@gmail.com>
2024-02-14 07:22:04 -0800

    chmoded

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,6 @@
 -- This script lists all the cities of California from the hbtn_0d_usa database without using the JOIN keyword
 
--- Select the state ID for California
-SET @california_id := (SELECT id FROM states WHERE name = 'California');
-
 -- List all cities of California
-SELECT *
-FROM cities
-WHERE state_id = @california_id
+SELECT * FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
