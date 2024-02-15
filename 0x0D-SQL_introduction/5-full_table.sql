463c7ce6a380b04fb757db0ac42a88b855a4ebd3
TheOrbMaverick <enangita@gmail.com>
2024-02-15 14:17:55 +0100

    going

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -4,11 +4,5 @@
 -- You can run this script using the cat command with mysql like this:
 -- cat 5-full_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 
--- First, we define a variable to hold the database name
-SET @db_name := 'hbtn_0c_0';
-
 -- Then we execute a query to retrieve the full table description
-SELECT CONCAT('Table: first_table', '\n', 'Create Table: ', Create_Table)
-FROM information_schema.tables
-WHERE table_schema = @db_name
-AND table_name = 'first_table';
+SHOW CREATE TABLE first_table
