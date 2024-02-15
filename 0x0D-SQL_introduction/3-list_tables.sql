d25f841774bfed4cbe3ce16ffba56206c51e1846
TheOrbMaverick <enangita@gmail.com>
2024-02-15 14:12:25 +0100

    checking

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -2,9 +2,9 @@
 
 -- Check if the correct number of arguments is provided
 -- Usage: mysql -u <username> -p <password> -h <hostname> <database_name> < 3-list_tables.sql
--- SET @db_name = '$$mysql$$';
+SET @db_name = '$$mysql$$';
 
 -- Query to list tables
 -- Retrieve table names from the information_schema.tables view
 -- Filter by the specified database name
-SELECT table_name FROM information_schema.tables WHERE table_schema = 'mysql';
+SELECT table_name FROM information_schema.tables WHERE table_schema = @db_name;
