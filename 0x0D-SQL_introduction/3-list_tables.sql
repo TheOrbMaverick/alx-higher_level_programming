14bd3597ae7a1ccd9fc1f3f5c461471a30e620ae
TheOrbMaverick <enangita@gmail.com>
2024-02-15 14:15:38 +0100

    checking

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,10 +1,6 @@
 -- Script to list all tables of a specified database in MySQL server
 
--- Check if the correct number of arguments is provided
--- Usage: mysql -u <username> -p <password> -h <hostname> <database_name> < 3-list_tables.sql
-SET @db_name = '$$mysql$$';
-
 -- Query to list tables
 -- Retrieve table names from the information_schema.tables view
 -- Filter by the specified database name
-SELECT table_name FROM information_schema.tables WHERE table_schema = @db_name;
+SHOW TABLES
