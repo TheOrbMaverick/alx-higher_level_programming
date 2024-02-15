79fbe2bbd520608f91d7b3bf207f7d42116f843c
TheOrbMaverick <enangita@gmail.com>
2024-02-15 18:09:27 +0100

    going

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,10 @@
 -- This script lists all shows from the hbtn_0d_tvshows_rate database by their rating sum
 
 -- List all shows and their rating sum
-SELECT title, SUM(rate) AS rating_sum
-    FROM tv_shows AS tvtitle
-        INNER JOIN tv_show_ratings AS rater
-        ON tvtitle.id = rater.show_id
-GROUP BY title
-ORDER BY rating_sum DESC;
+
+SELECT title, SUM(rate) AS rating
+  FROM tv_shows AS tite
+       INNER JOIN tv_show_ratings AS rates
+       ON tite.id = rates.show_id
+ GROUP BY title
+ ORDER BY rating DESC;
