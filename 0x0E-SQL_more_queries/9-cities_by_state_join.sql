637908baafa398cacb771ece26ae1883ea656634
TheOrbMaverick <enangita@gmail.com>
2024-02-15 16:14:34 +0100

    checking how

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 
 -- List all cities with their corresponding state names
 SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states ON states.id = cities.states_id
+INNER JOIN states ON states.id = cities.state_id
 ORDER BY cities.id;
