db5edb941782fe358a67e7ba39e71eb7aa853c0f
TheOrbMaverick <enangita@gmail.com>
2024-02-15 18:00:17 +0100

    going

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,10 +1,12 @@
 -- This script lists all shows without the genre Comedy from the hbtn_0d_tvshows database
 
--- Get the show IDs linked to the genre Comedy
-SET @comedy_show_ids := (SELECT GROUP_CONCAT(tv_shows.id) FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id WHERE tv_genres.name = 'Comedy');
-
 -- List all shows without the genre Comedy
 SELECT tv_shows.title
 FROM tv_shows
-WHERE tv_shows.id NOT IN (SELECT show_id FROM tv_show_genres WHERE show_id IN (SELECT * FROM FIND_IN_SET(show_id, @comedy_show_ids)))
+WHERE tv_shows.id NOT IN (
+    SELECT tv_shows.id FROM tv_shows 
+    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = "Comedy"
+    )
 ORDER BY tv_shows.title ASC;
