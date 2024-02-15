be2997a5589fddf027906d4452aadd48f45d71cb
TheOrbMaverick <enangita@gmail.com>
2024-02-15 18:19:21 +0100

    going

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,9 +2,11 @@
 
 -- List all genres and their rating sum
 SELECT name, SUM(rate) AS rating_sum
-FROM tv_genres AS gen
-INNER JOIN tv_show_genres AS shw ON shw.genre_id = gen.id
+    FROM tv_genres AS gen
+        INNER JOIN tv_show_genres AS shw
+        ON shw.genre_id = gen.id
 
-INNER JOIN tv_show_ratings AS rt ON rt.show_id = shw.show_id
+        INNER JOIN tv_show_ratings AS rt
+        ON rt.show_id = shw.show_id
 GROUP BY name
 ORDER BY rating_sum DESC;
