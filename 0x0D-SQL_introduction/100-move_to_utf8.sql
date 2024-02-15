579b6aacb420ee27fb350dedf9d45c445c8fb4f2
TheOrbMaverick <enangita@gmail.com>
2024-02-15 15:09:21 +0100

    checking it

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,12 @@
+-- Switch to the 'hbtn_0c_0' database
+-- This sets the default database context for subsequent SQL statements
+USE hbtn_0c_0;
+
 -- Change the default character set and collation for the 'hbtn_0c_0' database
 -- This ensures that all new tables created in this database will use UTF-8 encoding
 
 ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Change the character set and collation for the 'first_table' table
+-- This ensures that all existing data in this table is converted to UTF-8 encoding
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
