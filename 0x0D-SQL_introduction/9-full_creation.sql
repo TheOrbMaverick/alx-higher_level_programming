f7b66effd65748772d42b2c1d26f11d8b106cd9c
TheOrbMaverick <enangita@gmail.com>
2024-02-15 14:39:21 +0100

    checked smh

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,10 +2,13 @@
 -- The database name will be passed as an argument of the mysql command
 
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
+	id INT,
+	name VARCHAR(256),
+	score INT
 );
 
-INSTERT INTO second_table (id, name, score) 
-VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
