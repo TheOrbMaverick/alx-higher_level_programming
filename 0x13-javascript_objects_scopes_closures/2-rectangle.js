f5f3fae4146bca1c0b9297330cb90eae217afc37
TheOrbMaverick <enangita@gmail.com>
2024-03-12 18:15:50 +0100

    ch

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (arguments.length === 1 || w <= 0 || h <= 0) {
+    if (arguments.length !== 1 || w <= 0 || h <= 0) {
       return {};
     }
     this.width = w;
