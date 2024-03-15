87e99d550f85c0341e366970a2713f57ad0601db
TheOrbMaverick <enangita@gmail.com>
2024-03-15 19:39:08 +0100

    goto

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -52,8 +52,7 @@ def list_states(username, password, database):
         cursor.execute(query)
 
         # Fetch and display results
-        results = set(row for row in cursor.fetchall())
-        
+        results = cursor.fetchall()
         for row in results:
             print(row)
 
