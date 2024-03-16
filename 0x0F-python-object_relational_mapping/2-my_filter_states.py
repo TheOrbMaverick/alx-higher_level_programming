d663d63f0d25815c3f143a577b4533a68da23f51
TheOrbMaverick <enangita@gmail.com>
2024-03-16 13:15:39 +0100

    checking

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -32,8 +32,8 @@ def search_states(username, password, database, state_name):
         cursor = db.cursor()
 
         # Execute SQL query with user input
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-        cursor.execute(query, (state_name,))
+        query = (f"SELECT * FROM states WHERE name = {state_name} ORDER BY id ASC")
+        cursor.execute(query)
 
         # Fetch and display results
         results = cursor.fetchall()
