06e4b600176ee9b9dd379748184924ca9ac1e131
TheOrbMaverick <enangita@gmail.com>
2024-03-28 00:38:30 +0100

    checking

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-# The allowed methods are extracted from the Allow header in the response headers
-curl -si "$1" | grep -i "Allow:" | awk '{print $2}'
+# This script takes in a URL and displays all HTTP methods the server will accept
+
+# Check if URL argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 URL"
+    exit 1
+fi
+
+# Send an OPTIONS request to the URL and display the Allow header
+curl -s -i -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
