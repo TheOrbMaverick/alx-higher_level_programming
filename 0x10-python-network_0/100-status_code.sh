3993086b21d7c44efdeba09ad4c84e2ccf190e50
TheOrbMaverick <enangita@gmail.com>
2024-03-28 01:02:18 +0100

    chegs

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument and displays only the status code of the response
-curl -s -o temp_file "$1"; grep -oP '(?<=HTTP/1.1 )\d+' temp_file; rm temp_file
+curl -s -o temp_file "$1" | grep -oP '(?<=HTTP/1.1 )\d+' temp_file | rm temp_file
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Make a request to the target URL and redirect the response body to /dev/null
+# Make a request to the target URL with the correct data to trigger the desired response
 curl -sX PUT "http://0.0.0.0:5000/catch_me" -d "user_id=98" > /dev/null
