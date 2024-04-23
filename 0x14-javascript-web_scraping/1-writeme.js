10e73143e83f3a3f005ffa878dc06ff290c43e99
TheOrbMaverick <enangita@gmail.com>
2024-04-23 16:41:44 +0100

    why

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,11 +5,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, {encoding: 'utf-8', flag: 'w'}, (err) => {
+fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
     return;
   }
-
-  console.log('File written successfully!');
 });
