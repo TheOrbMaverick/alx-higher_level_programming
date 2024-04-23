d0ae7e2928c638ff294267f29dd1bcd8aefb90b1
TheOrbMaverick <enangita@gmail.com>
2024-04-23 17:19:58 +0100

    why

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,11 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request(url, (error) => {
+request(url, (error, response) => {
     if (error) {
         console.error(error);
+    }
+    else {
+        console.log('code: ' + response.statusCode);
     }
 });
